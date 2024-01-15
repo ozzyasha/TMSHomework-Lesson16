5d3c109bb5a68df3d60739726277151667c9f60c
Natallia Mazur <natashamazur@bk.ru>
2024-01-15 18:02:19 +0300

    Added new project for homework

---
+++ TMSHomework-Lesson16/TMSHomework-Lesson16/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TMSHomework-Lesson16
+//
+//  Created by Наталья Мазур on 15.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+// ДЗ:
+// Создать приложение со следующим функционалом:
+// Появление разноцветного кружочка, который по тапу исчезает и появляется в новом месте.
+
